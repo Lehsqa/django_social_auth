284cc9de4c3cab44073924c5a04ba1228cbcad7d
devRSV <devRSV@d.c>
2023-10-20 22:00:17 +0300

    BUG FIX: error path naming

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('facebook/login', views.FacebookLoginView.as_view(), name='facebook_login'),
-    path('google/login', views.GoogleLoginView.as_view(), name='google_login')
+    path('facebook/login/', views.FacebookLoginView.as_view(), name='facebook_login'),
+    path('google/login/', views.GoogleLoginView.as_view(), name='google_login'),
 ]
